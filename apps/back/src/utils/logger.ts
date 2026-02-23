2144e53788d4e80b12fd399c7c6c4948ef88b3e9
nada-r <nadarzer@gmail.com>
2026-02-23 23:24:32 +0100

    put back colors in logs

--- apps/back/src/utils/logger.ts
+++ apps/back/src/utils/logger.ts
@@ -2,5 +2,8 @@ import pino from "pino";
 
 export const logger = pino({
   level: "info",
-  transport: { target: "pino-pretty" },
+  transport: {
+    target: "pino-pretty",
+    options: { colorize: true, singleLine: true },
+  },
 });
